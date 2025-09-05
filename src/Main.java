0b1052a2cf152e9fa21470704d396a2fd81d69a2
Nathan Solano <nathanbelmonte.ns@gmail.com>
2025-09-05 13:41:06 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,9 +22,43 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devNathan = new Dev();
+        devNathan.setNome("Nathan");
+        devNathan.inscreverBotcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Nathan:" + devNathan.getConteudosInscritos());
+        devNathan.progredir();
+        devNathan.progredir();
+        System.out.println("-----------------------");
+        System.out.println("Conteúdos Concluidos Nathan:" + devNathan.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos Nathan:" + devNathan.getConteudosInscritos());
+        System.out.println("XP: "+ devNathan.calcularTotalXp());
+
+        System.out.println("-----------------------");
+
+        Dev devDyenis = new Dev();
+        devDyenis.setNome("Dyenis");
+        devDyenis.inscreverBotcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Dyenis:" + devDyenis.getConteudosInscritos());
+        devDyenis.progredir();
+        devDyenis.progredir();
+        devDyenis.progredir();
+        System.out.println("-----------------------");
+        System.out.println("Conteúdos Concluidos Dyenis:" + devDyenis.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos Dyenis:" + devDyenis.getConteudosInscritos());
+        System.out.println("XP: "+ devDyenis.calcularTotalXp());
+
+
 
     }
 }
